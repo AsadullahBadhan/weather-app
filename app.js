f44d6ca3c9fee134708c0c316f61bd3bd979e5cd
AsadullahBadhan <asadullah.badhan@gmail.com>
2022-12-24 20:53:13 +0600

    render data from current location on first load

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
 const searchBox = document.getElementById("search-box");
-const searchBtn = document.getElementById("search-btn");
-const placeName = document.getElementById("place-name");
-const temperature = document.getElementById("temperature");
-const weatherCondition = document.getElementById("weather");
-const weatherImg = document.getElementById("weather-img");
-const form = document.getElementById("place-form");
+const form = document.getElementById("search-form");
 
-let icon;
-let url = "https://api.openweathermap.org/data/2.5/weather?appid=2c0784a6ac7fdd50f8df729001f67b8d&units=metric&q=";
-let defaultUrl = url + 'dhaka';
+navigator.geolocation.getCurrentPosition((position) => {
+    defaulWeather(position.coords.latitude, position.coords.longitude)
+})
 
-window.addEventListener('DOMContentLoaded', callApi)
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let searchBoxValue = searchBox.value;
-    if (searchBoxValue.includes(" ")) {
-        searchBoxValue = searchBoxValue.replace(" ", "%20");
-        defaultUrl = url + searchBoxValue;
-    } else {
-        defaultUrl = url + searchBoxValue;
-    }
-    callApi();
-    searchBox.value = '';
-});
-
-function callApi() {
-    fetch(defaultUrl)
+function defaulWeather(lat, lon) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2c0784a6ac7fdd50f8df729001f67b8d&units=metric`;
+    fetch(url)
         .then((res) => res.json())
         .then((data) => getWeather(data));
 }
 
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?appid=2c0784a6ac7fdd50f8df729001f67b8d&units=metric&q=${searchBox.value}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => getWeather(data));
+    searchBox.value = '';
+});
+
 function getWeather(data) {
+    const weatherBody = document.getElementById('weather-body');
+
     //showing city name
-    placeName.innerText = data.name;
+    let placeName = data.name;
     // get temperature
     const mainData = data.main;
     const temp = Math.round(mainData.temp);
-    temperature.innerText = temp;
     //getCondition
     const weather = data.weather[0];
-    icon = weather.icon;
-    weatherCondition.innerText = weather.description;
+    const icon = weather.icon;
+    let weatherCondition = weather.description;
     //showing condition image
     let imageSrc = `https://openweathermap.org/img/wn/${icon}@4x.png`;
-    weatherImg.src = imageSrc;
-}
 
+    console.log(data)
+
+    weatherBody.innerHTML = `
+                <img src=${imageSrc} alt="weather" />
+				<h1>${placeName}</h1>
+				<h3>${temp}&deg;C</h3>
+				<h1 class="lead" id="weather">${weatherCondition}</h1>
+    `
+}
